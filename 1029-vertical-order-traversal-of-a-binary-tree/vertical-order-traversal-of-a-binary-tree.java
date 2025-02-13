2865118095886f4168cda6ac207e09642ccdffc2
A2rjav <114976831+A2rjav@users.noreply.github.com>
2025-02-13 09:50:11 +0530

    Time: 4 ms (42.77%) | Memory: 42.8 MB (22.23%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -18,40 +18,37 @@ class Solution {
         TreeNode node;
         int hd;
         int vd;
-        Info(TreeNode node,int hd,int vd){
+        public Info(TreeNode node,int hd, int vd){
             this.node = node;
             this.hd = hd;
             this.vd = vd;
-        }   
+        }
     }
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        TreeMap<Integer,TreeMap<Integer,List<Integer>>> map = new TreeMap<>();
         List<List<Integer>> ans = new ArrayList<>();
+        Map<Integer,TreeMap<Integer,ArrayList<Integer>>> map = new TreeMap<>();
         Queue<Info> q = new LinkedList<>();
         q.add(new Info(root,0,0));
         while(!q.isEmpty()){
-            int size= q.size(); 
-            for(int i=0;i<size;i++){
-                Info curr = q.remove();
-                TreeNode node = curr.node;
-                int hd = curr.hd;
-                int vd = curr.vd;
+            Info curr = q.remove();
+            TreeNode node = curr.node;
+            int hd = curr.hd;
+            int vd = curr.vd;
                 map.putIfAbsent(hd,new TreeMap<>());
                 map.get(hd).putIfAbsent(vd,new ArrayList<>());
                 map.get(hd).get(vd).add(node.val);
-                if(node.left!= null) q.add(new Info(node.left,hd-1,vd+1));
-                if(node.right!= null) q.add(new Info(node.right,hd+1,vd+1));
-            }
+                if(node.left != null) q.add(new Info(node.left,hd-1,vd+1));
+                if(node.right != null) q.add(new Info(node.right,hd+1,vd+1));
+            
         }
-        for (TreeMap<Integer, List<Integer>> verticalMap : map.values()) {
-            List<Integer> verticalList = new ArrayList<>();
-            for (List<Integer> nodes : verticalMap.values()) {
-                Collections.sort(nodes); 
-                verticalList.addAll(nodes);
+        for (TreeMap<Integer, ArrayList<Integer>> verticalMap : map.values()) {
+            List<Integer> list = new ArrayList<>();
+            for (ArrayList<Integer> nodes : verticalMap.values()) {
+                Collections.sort(nodes);
+                list.addAll(nodes);
             }
-            ans.add(verticalList);
+            ans.add(list);
         }
-            System.out.println(map);
         return ans;
     }
 }
