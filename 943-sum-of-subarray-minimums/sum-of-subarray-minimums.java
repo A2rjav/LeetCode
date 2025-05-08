f3ba10552fd7b5c060af2d106d79e9463b31479c
A2rjav <114976831+A2rjav@users.noreply.github.com>
2025-05-09 01:01:50 +0530

    Time: 65 ms (50.64%) | Memory: 52.7 MB (19.79%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,37 +1,39 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        int mod = 1000000007;
+        int mod = 1_000_000_007;
+        int n = arr.length;
+        int prevSmaller[] = new int[n];
+        int nextSmaller[] = new int[n];
 
-        int n=arr.length;
-        int [] prevSmaller = new int [n];
-        int [] nextSmaller = new int[n];
-
-        Stack<Integer> s = new Stack<>();
+        Stack<Integer> st = new Stack<>();
 
         for(int i=0;i<n;i++){
-            while(!s.isEmpty() && arr[i]<arr[s.peek()]){
-                s.pop();
+            while(!st.isEmpty() && arr[st.peek()]>arr[i]){
+                st.pop();
             }
-            prevSmaller[i] = s.isEmpty() ? -1: s.peek();
-            s.push(i);
+            prevSmaller[i] = st.isEmpty()? -1 : st.peek();
+            st.push(i);
         }
-        s.clear();
+
+        st.clear();
 
         for(int i=n-1;i>=0;i--){
-            while(!s.isEmpty() && arr[i] <= arr[s.peek()] ){
-                s.pop();
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]){
+                st.pop();
             }
-            nextSmaller[i] = s.isEmpty() ? n:s.peek();
-            s.push(i);
+            nextSmaller[i] = st.isEmpty() ? n:st.peek();
+            st.push(i);
         }
 
         long ans =0;
         for(int i=0;i<n;i++){
             long left = i - prevSmaller[i];
             long right = nextSmaller[i] - i;
-            ans = (ans + (left * right % mod) * arr[i]%mod)%mod;
+
+            ans = (ans + ((left * right% mod ) * arr[i]) %mod)%mod;
         }
 
-        return (int) ans;
+        return (int ) ans;
+
     }
 }
