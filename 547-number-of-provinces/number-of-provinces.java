ddb7933be544d8c50686ac13b657945a59cea610
A2rjav <114976831+A2rjav@users.noreply.github.com>
2025-05-28 11:57:59 +0530

    Time: 3 ms (34.72%) | Memory: 46.6 MB (91.39%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,23 +1,37 @@
 class Solution {
-    boolean vis[] ;
+    boolean vis[];
     int n;
+    ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
     public int findCircleNum(int[][] isConnected) {
-        n = isConnected.length;
-        int ans= 0;
+        n= isConnected.length;
         vis = new boolean[n];
-        for(int i =0;i<n;i++){
+        int ans=0;
+
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j] == 1 && i!=j){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                }
+            }
+        }
+        
+        for(int i=0;i<n;i++){
             if(!vis[i]){
                 ans++;
-                dfs(isConnected,i);
+                dfs(adj,i);
             }
         }
         return ans;
     }
-
-    public void dfs(int[][] isConnected, int curr){
+    public void dfs(ArrayList<ArrayList<Integer>> adj,int curr){
         vis[curr] = true;
-        for(int i =0;i<n;i++){
-            if(isConnected[curr][i] == 1 && !vis[i]) dfs(isConnected,i);
+        for(int i : adj.get(curr)){
+            if(!vis[i] ) dfs(adj,i);
         }
     }
 }
