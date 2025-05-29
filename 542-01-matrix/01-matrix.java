99ae2c6d769a9a0ebff3e63a5fe9f22ca33b7214
A2rjav <114976831+A2rjav@users.noreply.github.com>
2025-05-29 13:11:52 +0530

    Time: 13 ms (84.94%) | Memory: 47.5 MB (41.69%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,34 +1,37 @@
 class Solution {
+    int m,n;
+    boolean [][]vis;
     public int[][] updateMatrix(int[][] mat) {
-        int rows = mat.length;
-        int cols = mat[0].length;
-
-        Queue<int [] > q = new LinkedList<>();
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(mat[i][j] == 0) q.add(new int[]{i,j});
-                else  mat[i][j] = -1;
+        m = mat.length;
+        n = mat[0].length;
+        vis = new boolean[m][n];
+        int dist[][] = new int[m][n];
+        Queue<int[]> q = new LinkedList<>();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j] == 0){
+                    q.add(new int[]{i,j});
+                    vis[i][j] = true;
+                }
+                
             }
         }
 
-        int [][] direction= {{-1,0},{1,0},{0,1},{0,-1}};
+        int dirs[][]= {{-1,0},{1,0},{0,1},{0,-1}};
+
         while(!q.isEmpty()){
-            int[]cell = q.remove();
-            int r=cell[0], c= cell[1];
-            for(int []dir : direction){
-            int newRow = r+dir[0];
-            int newCol = c+dir[1];
+            int curr[] = q.poll();
+            for(int []dir: dirs){
+                int newR = dir[0] + curr[0];
+                int newC = dir[1] + curr[1];
 
-            if(newRow < 0 || newRow>=rows || newCol<0 || newCol >=cols || mat[newRow][newCol] != -1){
-                continue;
-            }
-
-            mat[newRow][newCol] = mat[r][c]+1;
-            q.add(new int[]{newRow,newCol});
-            
+                if(newR>=0 && newR <m && newC>=0 && newC < n && !vis[newR][newC]){
+                    dist[newR][newC] = dist[curr[0]][curr[1]] +1;
+                    q.add(new int[]{newR,newC});
+                    vis[newR][newC] = true;
+                } 
             }
         }
-        return mat;
+        return dist;
     }
-    
 }
