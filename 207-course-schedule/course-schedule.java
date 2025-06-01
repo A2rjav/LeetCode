93f17233ca86190e1854b92773fce2c3c43ff31f
A2rjav <114976831+A2rjav@users.noreply.github.com>
2025-06-01 22:34:36 +0530

    Time: 6 ms (74.12%) | Memory: 45.1 MB (76.99%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -8,29 +8,27 @@ class Solution {
 
         int indeg[] = new int[numCourses];
 
-        for(int prereq[] : prerequisites){
-            int task = prereq[0];
-            int pre = prereq[1];
-            adj.get(pre).add(task); //pre -> task
-            indeg[task]++;
-        }
+        for(int [] pre : prerequisites){
+            int u = pre[0];
+            int v = pre[1];
 
+            adj.get(u).add(v);
+            indeg[v]++;
+        }
         Queue<Integer> q = new LinkedList<>();
         for(int i=0;i<numCourses;i++){
             if(indeg[i] == 0) q.add(i);
         }
-
-        int completedCourses =0;
+        int cnt = 0;
         while(!q.isEmpty()){
-            int currentCourse = q.remove();
-            completedCourses++;
-
-            for(int neighbor : adj.get(currentCourse)){
+            int curr = q.poll();
+            cnt++;
+            for(int neighbor : adj.get(curr)){
                 indeg[neighbor]--;
                 if(indeg[neighbor] == 0) q.add(neighbor);
             }
         }
 
-        return completedCourses == numCourses;
+        return cnt == numCourses;
     }
 }
