9c8c8de0c7c3d38e3a6cb762fdb5f18baaf8f3a3
A2rjav <114976831+A2rjav@users.noreply.github.com>
2025-06-07 23:41:27 +0530

    Time: 4 ms (91.97%) | Memory: 61.2 MB (61.32%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -2,44 +2,51 @@ class Solution {
     int[] rank;
     int[] par;
     public int makeConnected(int n, int[][] connections) {
-        par = new int[n];
+        int wires = 0;
         rank = new int[n];
+        par = new int[n];
 
-        for(int i=0;i<n;i++)
-        par[i] = i;
+        for(int i=0;i<n;i++) par[i] = i;
+        for(int [] connection : connections){
+            int u = connection[0];
+            int v = connection[1];
 
-        int cntExtra = 0;
-        int m= connections.length;
-        for(int i=0;i<m;i++){
-            int u= connections[i][0];
-            int v = connections[i][1];
-
-            if(find(u) == find(v)) cntExtra++;
-            else union(u,v);
+            int parU = find(u);
+            int parV = find(v);
+            
+            if(parU == parV){
+                wires++;
+            }
+            else 
+            union(parU,parV); 
         }
-
-        int cntC =0;
+        int cntC = 0;
         for(int i=0;i<n;i++){
-            if(find(i) == i) cntC++;
+            if(find(i) == i ) cntC++;
         }
-        int ans = cntC-1;
-        if(cntExtra >= ans ) return ans;
+
+        int wiresNeeded = cntC-1;
+        if(wires>=wiresNeeded) return wiresNeeded;
         return -1;
     }
     public int find(int x){
         if(par[x] == x) return x;
         return par[x] = find(par[x]);
     }
-    public void union(int a,int b){
-        int parA = find(a);
-        int parB= find(b);
 
-        if(rank[parA] < rank[parB])
-        par[parA] = parB;
-        else if(rank[parA] > rank[parB])
-        par[parB] = parA;
-        else 
-        par[parB] = parA;
-        rank[parA]++;
+    public void union(int x,int y){
+        int parX = find(x);
+        int parY = find(y);
+
+        if(rank[parX]> rank[parY]){
+            par[parY] = parX;
+        }
+        else if(rank[parX]< rank[parY]){
+            par[parX] = parY;
+        }
+        else{
+            par[parX] = parY;
+            rank[parX]++;
+        }
     }
 }
