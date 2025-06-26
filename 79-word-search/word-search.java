c63487d0897390af1613ef1dfcb705208a8352ed
A2rjav <114976831+A2rjav@users.noreply.github.com>
2025-06-27 01:27:16 +0530

    Time: 130 ms (86.72%) | Memory: 41.6 MB (62.22%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,33 +1,29 @@
 class Solution {
-    public boolean dfs(char[][] board, String word, int i,int j,int idx){
-        if(idx == word.length()){
-            return true;
-        }
-
-        if(i<0 || i>=board.length || j<0 || j>= board[0].length || board[i][j] != word.charAt(idx)){
-            return false;
-        }
-
-        char temp = board[i][j];
-        board[i][j] = '#';
-
-        boolean found = dfs(board,word,i+1,j,idx+1) ||
-        dfs(board,word,i-1,j,idx+1) ||
-        dfs(board,word,i,j+1,idx+1) ||
-        dfs(board,word,i,j-1,idx+1);
-
-        board[i][j] = temp;
-        return found;
-    }
+    int len,m,n;
     public boolean exist(char[][] board, String word) {
-        if(board == null || board.length == 0) return false;
-        int rows = board.length,cols = board[0].length;
+        len = word.length();
+        m = board.length;
+        n = board[0].length;
 
-        for(int i=0;i<rows;i++){
-            for(int j=0;j< cols;j++){
-                if(dfs(board,word,i,j,0)) return true;
+        for(int i=0;i<m;i++)
+        for(int j=0;j<n;j++){
+            if(backtrack(board,word,i,j,0)){
+                return true;
             }
         }
         return false;
+    }
+    public boolean backtrack(char[][] board,String word,int i,int j,int idx){
+        if(idx==len) return true;
+
+        if(i<0 || j<0 || i>=m || j>=n || board[i][j] != word.charAt(idx)) return false; 
+        char temp = board[i][j];
+        board[i][j] = '#'; 
+
+        boolean found = backtrack(board,word,i+1,j,idx+1)||backtrack(board,word,i-1,j,idx+1)||backtrack(board,word,i,j+1,idx+1)||backtrack(board,word,i,j-1,idx+1);
+        
+        board[i][j] = temp;
+        return found;
+            
     }
 }
