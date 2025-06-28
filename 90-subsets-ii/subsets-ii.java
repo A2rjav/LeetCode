def6675218b571a9a485a15602888563c3bf7303
A2rjav <114976831+A2rjav@users.noreply.github.com>
2025-06-29 01:54:08 +0530

    Time: 1 ms (99.89%) | Memory: 43.8 MB (19.18%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,24 +1,25 @@
 class Solution {
-    Set<List<Integer>> ans = new HashSet<>();
     int n;
+    List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
         n = nums.length;
+        Arrays.sort(nums);
         backtrack(nums,0,new ArrayList<>());
-        System.out.println(ans);
-        List<List<Integer>> res = new ArrayList<>();
-        for(List<Integer> curr : ans) res.add(new ArrayList<>(curr));
-        return res;
+        return ans;
     }
-    public void backtrack(int []nums,int i,ArrayList<Integer> list){
-        if(i==n){
+
+    public void backtrack(int []nums,int i,List<Integer> list){
+        if(i==n) {
             ans.add(new ArrayList<>(list));
             return;
         }
-
-        backtrack(nums,i+1,list);
         list.add(nums[i]);
         backtrack(nums,i+1,list);
         list.remove(list.size()-1);
+        
+        while(i+1 < n && nums[i] == nums[i+1]) i++;
+
+        backtrack(nums,i+1,list);
+        
     }
 }
