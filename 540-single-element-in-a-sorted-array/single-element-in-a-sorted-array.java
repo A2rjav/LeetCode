403c37ba8feb13d17eba1a5adc75b10cfd208b10
A2rjav <114976831+A2rjav@users.noreply.github.com>
2025-07-07 01:50:52 +0530

    Time: 0 ms (100.00%) | Memory: 50.3 MB (52.75%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,24 +1,23 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int l = 0,n = nums.length;
-        int r = n-1;
-
+        int s=1;
+        int e = nums.length-2;
+        int n = nums.length;
         if(n==1) return nums[0];
-        if(nums[0] != nums[1]) return nums[0];
+        if(nums[1] != nums[0]) return nums[0];
         if(nums[n-1] != nums[n-2]) return nums[n-1];
 
-        while(l<=r){
-            int m = (l + r)>>1;
+        while(s<=e){
+            int mid = (s + e)>>1;
+            if(nums[mid]!= nums[mid+1] && nums[mid] != nums[mid-1])
+            return nums[mid];
 
-            if(nums[m] != nums[m-1] && nums[m] != nums[m+1])
-            return nums[m];
-
-            if((m%2 == 1 && nums[m-1] == nums[m]) || (m%2==0 && nums[m] == nums[m+1])){
-                l = m+1;
-            }else r = m-1;
-            
+            if((mid%2== 0 && nums[mid-1] == nums[mid] )|(mid%2 ==1 && nums[mid+1] == nums[mid]) ){
+                e = mid-1;
+            }else{
+                s = mid+1;
+            }
         }
-
         return -1;
     }
 }
