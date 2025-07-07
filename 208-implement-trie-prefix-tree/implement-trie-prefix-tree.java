ca375144baefc4e3cf9985dc811575ef1fcb4f0c
A2rjav <114976831+A2rjav@users.noreply.github.com>
2025-07-08 00:28:06 +0530

    Time: 34 ms (71.22%) | Memory: 55.3 MB (84.70%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,42 +1,40 @@
 class Trie {
     class TrieNode{
         TrieNode[] children = new TrieNode[26];
-        boolean isEnd;
+        boolean eow = false;
     }
     TrieNode root;
     public Trie() {
-         root = new TrieNode();
+        root = new TrieNode();
     }
     
     public void insert(String word) {
-        TrieNode curr = root;
-        for(char ch:word.toCharArray()){
-            int idx = ch-'a';
-            if(curr.children[idx] == null){
-                curr.children[idx] = new TrieNode();
-            }
+        TrieNode curr= root;
+        for(int level = 0;level<word.length();level++){
+            int idx = word.charAt(level)-'a';
+            if(curr.children[idx] == null)
+            curr.children[idx] = new TrieNode();
             curr = curr.children[idx];
         }
-        curr.isEnd = true;
+        curr.eow = true;
     }
     
     public boolean search(String word) {
-        TrieNode curr= root;
-        for(char ch : word.toCharArray()){
-            int idx = ch-'a';
-            if(curr.children[idx] == null ) {
-                return false;
-            }
+        TrieNode curr = root;
+        for(int level =0;level<word.length();level++){
+            int idx = word.charAt(level)-'a';
+            if(curr.children[idx]==null)
+            return false;
             curr = curr.children[idx];
         }
-        return curr.isEnd;
+        return curr.eow == true;
     }
     
-    public boolean startsWith(String word) {
-        TrieNode curr = root;
-        for(char ch : word.toCharArray()){
+    public boolean startsWith(String prefix) {
+        TrieNode curr= root;
+        for(char ch : prefix.toCharArray()){
             int idx = ch-'a';
-            if(curr.children[idx] == null)return false;
+            if(curr.children[idx] == null ) return false;
             curr = curr.children[idx];
         }
         return true;
