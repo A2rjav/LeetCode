1634cea717f7c5c33f0d1507e8f3a5a0091476bb
A2rjav <114976831+A2rjav@users.noreply.github.com>
2025-07-12 01:50:45 +0530

    Time: 8 ms (86.11%) | Memory: 47.2 MB (6.59%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,29 +1,19 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        int n =intervals.length;
+        if(n==1) return intervals;
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        ArrayList<int[]> list = new ArrayList<>();
+        list.add(intervals[0]);
 
-        // for(int [] arr : intervals){
-        //     Arrays.sort(arr);   
-        // } -> wrong because internally it is already sorted just we need to sort on the basis of starting element
-
-        Arrays.sort(intervals,(a,b) -> Integer.compare(a[0],b[0]));
-        ArrayList<int [] > list = new ArrayList<>();
-
-        int [] currentInterval = intervals[0];
-        list.add(currentInterval);
-
-        for(int i=1;i<intervals.length;i++){
-            int currentEnd = currentInterval[1];
-            int nextStart = intervals[i][0];
-            int nextEnd = intervals[i][1];
-
-            if(intervals[i][0] <= currentInterval[1]){
-                currentInterval[1] = Math.max(currentEnd, nextEnd);
-            }else{
-                currentInterval = intervals[i];
-                list.add(currentInterval);
-            }
+        for(int i=1;i<n;i++){
+            int curr[] = intervals[i];
+            int last[] = list.get(list.size()-1);
+            if(curr[0]<=last[1]){
+                last[1] = Math.max(curr[1],last[1]);
+            }else
+            list.add(intervals[i]);
         }
-        int[][] ans = list.toArray(new int[list.size()][]);
-        return ans;
+        return list.toArray(new int[list.size()][]);
     }
 }
