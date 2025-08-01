19611167840814546e103452bc870fb81d82be97
A2rjav <114976831+A2rjav@users.noreply.github.com>
2025-08-02 00:28:13 +0530

    Time: 1 ms (100.00%) | Memory: 44.7 MB (32.52%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -14,23 +14,21 @@
  * }
  */
 class Solution {
-    TreeNode first = null,second = null;
+    TreeNode first = null;
+    TreeNode sec = null;
     TreeNode prev = new TreeNode(Integer.MIN_VALUE);
     public void recoverTree(TreeNode root) {
         inorder(root);
         int temp = first.val;
-        first.val = second.val;
-        second.val = temp;
+        first.val = sec.val;
+        sec.val= temp;
     }
-
     public void inorder(TreeNode root){
-        if(root == null ) return;
-
+        if(root == null) return;
         inorder(root.left);
-        if(first == null && prev.val > root.val) first = prev;
-        if(first != null && prev.val > root.val) second = root;
-        prev = root;
+        if(first == null && prev.val>root.val) first = prev;
+        if(first != null && prev.val > root.val) sec = root;
+        prev= root;
         inorder(root.right);
     }
-    
 }
