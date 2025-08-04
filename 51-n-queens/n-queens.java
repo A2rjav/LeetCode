2391b7b3bdd3ac50010255f39b2ac32c09558fc4
A2rjav <114976831+A2rjav@users.noreply.github.com>
2025-08-04 14:40:57 +0530

    Time: 2 ms (86.57%) | Memory: 45 MB (55.83%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,52 +1,47 @@
 class Solution {
-    List<List<String>> ans = new ArrayList<>();
-    int n;
+    List<List<String>> res = new ArrayList<>();
     public List<List<String>> solveNQueens(int n) {
-        this.n= n;
-        char [][]board = new char[n][n];
-        for(int i=0;i<n;i++) Arrays.fill(board[i],'.');
-        backtrack(board,0,n);
-        return ans;
-        
+        char board[][] = new char[n][n];
+        for(char[]row:board)
+        Arrays.fill(row,'.');
+        backtrack(board,0);
+        return res;
     }
-    public boolean isValid(int row,int col,char [][]board){
-        // up
-        for(int i=row-1;i>=0;i--){
-            if(board[i][col] == 'Q')return false;
-        }
-        // left
-        for(int i=0;i<col;i++) 
-        if(board[row][i] == 'Q') 
-        return false;
+    public void backtrack(char[][]board,int row){
+        int n= board.length;
+        if(row==n){
+            ArrayList<String> list = new ArrayList<>();
+            for(char[] r : board)
+            list.add(new String(r));
 
-        // diagonal
-        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
-            if(board[i][j] == 'Q') 
-            return false;
-        }
-        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++){
-            if(board[i][j] == 'Q') 
-            return false;
-        }
-
-        return true;
-
-    }
-    public void backtrack(char[][]board,int row,int n){
-        if(row == n ){
-            List<String> curr = new ArrayList<>();
-            for(char[]b : board){
-                curr.add(new String(b));
-            }
-            ans.add(curr);
+            res.add(new ArrayList<>(list));
             return;
         }
+
         for(int col=0;col<n;col++){
-            if(isValid(row,col,board)){
-                board[row][col] ='Q';
-                backtrack(board,row+1,n);
-                board[row][col] ='.';
+            if(isSafe(row,col,board)){
+                board[row][col] = 'Q';
+                backtrack(board,row+1);
+                board[row][col] = '.';
             }
         }
+    }
+    boolean isSafe(int row,int col,char[][]board){
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col] == 'Q') return false;
+        }
+        int n = board.length;
+
+        // for(int i=0;i<col;i++)
+        // if(board[row][i] == 'Q') return false;
+        
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+            if(board[i][j] == 'Q') return false;
+        }
+        
+        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++){
+            if(board[i][j] == 'Q') return false;
+        }
+        return true;
     }
 }
