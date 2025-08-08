5c2adc57bbd2232cd26b86f9f22f2294c4001ead
A2rjav <114976831+A2rjav@users.noreply.github.com>
2025-08-09 00:11:16 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (35.82%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,13 +1,13 @@
 class Solution {
     Integer dp[][];
     public int uniquePaths(int m, int n) {
-        dp = new Integer[m][n];
-        return countWays(m-1,n-1,dp);
+        dp = new Integer[m+1][n+1];
+        return recurse(m-1,n-1);
     }
-    public int countWays(int m,int n,Integer [][]dp){
+    public int recurse(int m,int n){
         if(m<0 || n<0) return 0;
-        if(m==0 && n==0) return 1;
+        if(m==0 && n==0 ) return 1;
         if(dp[m][n] != null ) return dp[m][n];
-        return dp[m][n] = countWays(m-1,n,dp) + countWays(m,n-1,dp);
+        return dp[m][n] = recurse(m-1,n) + recurse(m,n-1);
     }
 }
