bcf6bdc1dada43f4e696526e120486e40341b32d
A2rjav <114976831+A2rjav@users.noreply.github.com>
2025-08-12 01:51:37 +0530

    Time: 20 ms (81.01%) | Memory: 47.1 MB (5.44%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,22 +1,20 @@
 class Solution {
+    int n;
     Integer dp[][];
     public int minInsertions(String s) {
-        int n = s.length();
+        n = s.length();
         dp = new Integer[n+1][n+1];
-        int lps = lps(0,n-1,s);
-        return n-lps;
+        return recurse(s,0,n-1);
     }
-    public int lps(int i,int j,String s){
-        if(i>j )return 0;
-        if(i==j) return 1;
+    public int recurse(String s,int i,int j){
+        if(i>j) return 0;
+        if(i==j) return 0;
 
-        if(dp[i][j] != null) return dp[i][j];
-        if(s.charAt(i) == s.charAt(j)){
-            return dp[i][j] = 2 + lps(i+1,j-1,s);
-        }else{
-            int a = lps(i+1,j,s);
-            int b = lps(i,j-1,s);
-            return dp[i][j] = Math.max(a,b);
+        if(dp[i][j] != null ) return dp[i][j];
+        if(s.charAt(i) == s.charAt(j)) dp[i][j] = recurse(s,i+1,j-1);
+        else{
+             dp[i][j] = Math.min(recurse(s,i+1,j)+1,recurse(s,i,j-1)+1);
         }
+        return dp[i][j];
     }
 }
