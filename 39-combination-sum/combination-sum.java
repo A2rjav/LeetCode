58b9b83beaf706df781a8cc003932d06b7de92ca
Aarjav Jain <114976831+A2rjav@users.noreply.github.com>
2025-10-05 23:59:53 +0530

    Time: 2 ms (84.46%) | Memory: 44.8 MB (46.09%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,26 +1,25 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-    public void util(int[] arr, int idx, int target, List<List<Integer>> ans, List<Integer> ds) {
-        if (idx == arr.length) {
-            if (target == 0) {
-                ans.add(new ArrayList<>(ds));
-            }
-            return;
-        }
-
-        if (arr[idx] <= target) {
-            ds.add(arr[idx]);
-            util(arr, idx, target - arr[idx], ans, ds);
-            ds.remove(ds.size() - 1);
-        }
-        util(arr, idx + 1, target, ans, ds);
-    }
-
+    int n;
+    List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        util(candidates, 0, target, ans, new ArrayList<>());
-        return ans;
+        n = candidates.length;
+
+         recurse(candidates, target, 0,0, new ArrayList<>());
+         return ans;
+    }
+    public void recurse(int[] candidates, int target, int i, int sum, ArrayList<Integer> list){
+        if(sum == target){
+                 ans.add(new ArrayList<>(list));
+                 return;
+        }
+        if( sum > target ||i == n){
+             return;
+        }
+        // if(candidates[i] + sum > target) return;
+        
+        list.add(candidates[i]);
+        recurse(candidates,target, i, sum + candidates[i], list);
+        list.remove(list.size()-1);
+        recurse(candidates,target, i+1, sum , list);
     }
 }
