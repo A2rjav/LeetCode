1a564361731b555970ec315b200ce364475b8c36
Aarjav Jain <114976831+A2rjav@users.noreply.github.com>
2025-10-07 03:20:15 +0530

    Time: 6 ms (99.22%) | Memory: 57.2 MB (22.36%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -3,27 +3,27 @@ class Solution {
     int n;
     public List<List<String>> partition(String s) {
         n = s.length();
-        ArrayList<String> list = new ArrayList<>();
-        recurse(s,list,0);
+        recurse(s,0,new ArrayList<>());
         return ans;
+
     }
-    public void recurse(String s,ArrayList<String> list,int idx){
-        if(idx == n){
+    public void recurse(String s, int start, ArrayList<String> list){
+        if(start == n){
             ans.add(new ArrayList<>(list));
             return;
         }
-        for(int i=idx;i<n;i++){
-            if(isPalindrome(s,idx,i)){
-                list.add(s.substring(idx,i+1));
-                recurse(s,list,i+1);
+        for(int i = start;i<s.length();i++){
+            if(isPalindrome(s,start,i)){
+                list.add(s.substring(start,i+1));
+                recurse(s,i+1,list);
                 list.remove(list.size()-1);
             }
         }
     }
-    public boolean isPalindrome(String s,int i,int j){
-        while(i<=j){
-            if(s.charAt(i++) != s.charAt(j--))
-            return false;
+    public boolean isPalindrome(String s,int i, int j){
+        while(i<j){
+            if(s.charAt(i++) != s.charAt(j--)) return false;
+            
         }
         return true;
     }
