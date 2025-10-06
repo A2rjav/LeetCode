0e2aa99404088059889d338e3ed60ba2414495d8
Aarjav Jain <114976831+A2rjav@users.noreply.github.com>
2025-10-07 01:58:53 +0530

    Time: 3 ms (71.24%) | Memory: 43.6 MB (23.71%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,28 +1,31 @@
 class Solution {
-    public void util(int [] arr,int idx, int target,List<List<Integer>> ans, List<Integer> ds){
+    List<List<Integer>> ans = new ArrayList<>();
+    int n;
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        n = candidates.length;
+        Arrays.sort(candidates);
+        recurse(candidates, target, 0, new ArrayList<>(), 0);
+
+        return ans;
+    }
+    public void recurse(int[] candidates, int target, int start, ArrayList<Integer> list, int sum){
         
-        if(target == 0){
-            ans.add(new ArrayList<> (ds) );
+        if(sum == target) {
+            ans.add(new ArrayList<>(list));
             return;
         }
-            
-        for(int i=idx;i<arr.length;i++){
-            if(i>idx && arr[i] ==arr[i-1]){
-                continue;
-            }
-            if(arr[i] > target){
-                break;
-            }
-            ds.add(arr[i]);
-            util(arr,i+1,target - arr[i],ans,ds);
-            ds.remove(ds.size() -1);
+        if(sum >target){
+            return;
+        }
+
+        for(int i= start;i<n;i++){
+            if(i > start && candidates[i] == candidates[i-1]) continue;
+            if(sum + candidates[i]> target ) break;
+
+            list.add(candidates[i]);
+            recurse(candidates,target,i+1, list, sum + candidates[i]);
+            list.remove(list.size()-1);
         }
         
-    }
-    public List<List<Integer>> combinationSum2(int[] arr, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(arr);
-        util(arr,0,target,ans,new ArrayList<>());
-        return ans;
     }
 }
